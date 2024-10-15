251f698c0d0078fbef0f1d993418b5b8c5ca2721
= <=>
2024-10-15 16:18:19 +0200

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿#if DEBUG
+int start = 0;
+int end = 10;
+int increase = 1;
+#endif
+
+#if RELEASE
+int start = 0;
+int end = 20;
+int increase = 1
+#endif
+
+for (int i = start; i <= end; i += increase)
+{
+    Console.WriteLine(i);
+}
